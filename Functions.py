1b88c4c6d3fbb2d9b14bdcdc0456db8d0b8596ea
Shreyash-Kadam-SK <kadam.shreyash.d@gmail.com>
2024-07-16 14:12:19 +0530

    Corrected typos

--- Functions.py
+++ Functions.py
@@ -1,7 +1,10 @@
 import Dictionary as d
+
+
 class CurrencyConverter:
     def __init__(self, currency_dict):
         self.currency_dict = currency_dict
+
     def input_currency(self):
         while True:
             user_choiceinput = input("Enter the currency you want to input: ")
@@ -10,23 +13,23 @@ class CurrencyConverter:
                     user_amt = input("Enter the amount you want to convert: ")
                     if user_amt.isdigit():
                         ua = int(user_amt)
-                        refrence_value = ua / self.currency_dict[user_choiceinput]
+                        reference_value = ua / self.currency_dict[user_choiceinput]
                         break
                     else:
                         print("Enter a valid integer.")
                 for i in range(len(d.currencies)):
                     if user_choiceinput != d.currencies[i]:
                         d.currenciesoutput_list.append(d.currencies[i])
-                return refrence_value
+                return reference_value
             else:
                 print(f"Currency '{user_choiceinput}' not found in the list.")
 
     def output_currency(self, reference_value):
         while True:
-            user_choiceoutput = input("Enter the currency for the output value: ")
-            if user_choiceoutput in self.currency_dict:
-                output_value = reference_value * self.currency_dict[user_choiceoutput]
-                print(f"Converted value: {output_value:.2f} {user_choiceoutput}")
+            user_choice_output = input("Enter the currency for the output value: ")
+            if user_choice_output in self.currency_dict:
+                output_value = reference_value * self.currency_dict[user_choice_output]
+                print(f"Converted value: {output_value:.2f} {user_choice_output}")
                 break
             else:
-                print(f"Currency '{user_choiceoutput}' not found in the list.")
+                print(f"Currency '{user_choice_output}' not found in the list.")
