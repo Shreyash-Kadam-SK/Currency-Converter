d2a9354774a349ef6bb6b53b51d63c854d9364ac
Shreyash-Kadam-SK <kadam.shreyash.d@gmail.com>
2024-07-16 13:09:19 +0530

    removed unnecessary stuff

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,3 @@
-currency_dict = {'Rupees': 10.00, 'Dollar': 0.12, 'BTC': 0.0000019, 'CAD': 0.16, 'NZD': 0.20, 'EGP': 5.73, 'AED': 0.44, 'AUD': 0.18, 'SK': 20.0}
+currency_dict = {'Rupees': 10.00, 'Dollar': 0.12, 'BTC': 0.0000019, 'CAD': 0.16, 'NZD': 0.20, 'EGP': 5.73, 'AED': 0.44, 'AUD': 0.18}
 currencies = ['Rupees', 'Dollar', 'BTC', 'CAD', 'NZD', 'EGP', 'AED', 'AUD']
 currenciesoutput_list = []
